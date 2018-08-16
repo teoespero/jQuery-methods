8d783fcb0f4fcbaeb8163dd84b7bfe8500d75415
teoespero <tespero@wgu.edu>
2018-08-16 16:57:06 -0700

    val(), html(), attr() methods used to validate a bootstrao form

--- val-method.js
+++ val-method.js
@@ -4,14 +4,48 @@
 
 // #Web-Dev-Bootcamp-Udemy
 
-$("#cars").change(function(){
-	var str = "";
-	$( "select option:selected" ).each(function() {
-      str += $(this).text() + " ";
-    });
-    $("#userChoice").text(str);
+
+
+
+$("#enterusername").change(function(){
+	var emailAddress = $("#enterusername").val();
+	if (!validateEmail(emailAddress)){
+		$("#messageEmail").html("The email you have entered is not valid.");
+		$("#messageEmail").attr("class", "text-danger");
+	}
+	else {
+		$("#messageEmail").html("");	
+	}
 });
 
-$("button").click(function (){
-	location.reload();
+$("#confEmailAddress").change(function(){
+	var confirmedEmailAddress = $("#confEmailAddress").val();
+	if (!validateEmail(confirmedEmailAddress)){
+		$("#messageConf").html("The email you have entered is not valid.");
+		$("#messageConf").attr("class", "text-danger");
+	}
+	else {
+		$("#messageConf").html("");	
+	}
 });
+
+
+$("button").click(function(){
+	var confirmedEmailAddress = $("#confEmailAddress").val();
+	var emailAddress = $("#enterusername").val();
+
+	if (emailAddress != confirmedEmailAddress){
+		$("#messageConf").html("The emails you have entered do not match.");
+		$("#messageConf").attr("class", "text-danger");
+	}
+	else {
+		$("#messageConf").html("");
+	}
+});
+
+
+function validateEmail(Email) {
+    var pattern = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
+    return $.trim(Email).match(pattern) ? true : false;
+}
