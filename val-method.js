a4f944d16b409d7235ceff0264dcf1ce3664537a
teoespero <tespero@wgu.edu>
2018-08-16 17:25:54 -0700

    added validations to email and pw using regex

--- val-method.js
+++ val-method.js
@@ -19,6 +19,7 @@ $("#enterusername").change(function(){
 });
 
 $("#confEmailAddress").change(function(){
+	$("button").html("Register")
 	var confirmedEmailAddress = $("#confEmailAddress").val();
 	if (!validateEmail(confirmedEmailAddress)){
 		$("#messageConf").html("The email you have entered is not valid.");
@@ -29,23 +30,53 @@ $("#confEmailAddress").change(function(){
 	}
 });
 
+$("#enterpassword").change(function(){
+	var userPassword = $("#enterpassword").val();
+	if (!CheckPassword(userPassword)){
+		$("#messagePW").html("Password is invalid. Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters");
+		$("#messageConf").attr("class", "text-danger");
+	}
+	else {
+		$("#messagePW").html("");
+	}
+});
+
 
 $("button").click(function(){
 	var confirmedEmailAddress = $("#confEmailAddress").val();
 	var emailAddress = $("#enterusername").val();
+	var userPassword = $("#enterpassword").val();
 
-	if (emailAddress != confirmedEmailAddress){
-		$("#messageConf").html("The emails you have entered do not match.");
-		$("#messageConf").attr("class", "text-danger");
+	if (CheckPassword(userPassword)){
+		if (emailAddress != confirmedEmailAddress){
+			$("#messageConf").html("The emails you have entered do not match.");
+			$("#messageConf").attr("class", "text-danger");
+			$("button").html("Validate again")
+		}
+		else {
+			$("#messageConf").html("");
+			$("#registered").html("You are now registered.");
+			$("#registered").attr("class", "text-success");
+			$("#messagePW").html("");
+		}
 	}
 	else {
-		$("#messageConf").html("");
+		$("#messagePW").html("Password is invalid. Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters");
+		$("#messageConf").attr("class", "text-danger");
 	}
 });
 
 
 function validateEmail(Email) {
-    var pattern = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    var pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     return $.trim(Email).match(pattern) ? true : false;
+}
+
+function CheckPassword(inputtxt) 
+{ 
+	// at least one number, one lowercase and one uppercase letter
+    // at least six characters
+	var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    return re.test(inputtxt);
 }
